89d84178570ebbed3ebf4e09c1a8e58ef53a87f8
Ananya <72216605+ananyachibber21@users.noreply.github.com>
2021-07-08 23:18:11 +0530

    Create String-tokens.java

---
+++ HackerRank/Java/Strings/String-tokens.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        
+        if (s.length() > 0) {
+        String[] tokens = s.split("[!,?._'@\\s]+");
+        System.out.println(tokens.length);
+        for(String token : tokens) {
+            System.out.println(token);
+        }
+    } else {
+        System.out.println(0);
+    }
+        scan.close();
+        
+    }
+}
+

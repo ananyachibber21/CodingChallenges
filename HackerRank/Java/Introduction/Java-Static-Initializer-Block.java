dba8b49b52b3594dc9db7dfe40e98e8c0552ec12
Ananya <72216605+ananyachibber21@users.noreply.github.com>
2021-07-15 12:04:03 +0530

    Update Java-Static-Initializer-Block.java

--- HackerRank/Java/Introduction/Java-Static-Initializer-Block.java
+++ HackerRank/Java/Introduction/Java-Static-Initializer-Block.java
@@ -4,26 +4,24 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+class InitializerBlock {
 
-static Scanner sc = new Scanner(System.in);
-static int B = sc.nextInt();
-static int H = sc.nextInt();
-static boolean flag;
+    static int B;
+    static int H;
+    static boolean flag;
 
-static{
-    try{
-    if(B>=0 && H>=0){
-        flag = true;
+    static {
+        Scanner scan = new Scanner(System.in);
+        B = scan.nextInt();
+        H = scan.nextInt();
+        scan.close();
+        if (B <= 0 || H <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
+        } else {
+            flag = true;
+        }
     }
-    else{
-        throw new Exception("Breadth and height must be positive");
-    }
-}
-catch(Exception e){
-    System.out.println(e);
-}
-}
 
 public static void main(String[] args){
 		if(flag){
@@ -34,4 +32,3 @@ public static void main(String[] args){
 	}//end of main
 
 }//end of class
-

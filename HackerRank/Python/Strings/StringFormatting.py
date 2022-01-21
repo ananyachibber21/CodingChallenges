2bfebf9572cfe02c791c25f9e3dc7b7cd45576ff
Ananya <ananyaunique21@gmail.com>
2022-01-21 21:20:49 +0530

    Strings with Python

--- HackerRank/Python/Strings/StringFormatting.py
+++ HackerRank/Python/Strings/StringFormatting.py
@@ -1,7 +1,11 @@
 def print_formatted(number):
+    width = len(bin(number)[2:])
     for i in range(1, number+1):
-        print("{}\t{}\t{}\t{}".format(str(i), str(oct(i)[2:]), str(hex(i)[2:]).upper(), str(bin(i)[2:])))
-
+        d = str(i)
+        o = str(oct(i)[2:])
+        h = str(hex(i)[2:]).upper()
+        b = str(bin(i)[2:])
+        print(d.rjust(width), o.rjust(width), h.rjust(width), b.rjust(width))
 if __name__ == '__main__':
     n = int(input())
     print_formatted(n)

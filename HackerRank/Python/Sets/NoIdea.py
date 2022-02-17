20b2fc053f32aff835cb2c75ca1cc3a0c46f1353
Ananya <ananyaunique21@gmail.com>
2022-02-17 18:30:41 +0530

    Sets in Python

--- HackerRank/Python/Sets/NoIdea.py
+++ HackerRank/Python/Sets/NoIdea.py
@@ -1,11 +1,13 @@
 n,m = list(map(int, input().split()))
 arr = list(map(int, input().split()))
-x = set(arr)
 A = set(map(int, input().split()))
 B = set(map(int, input().split()))
-a = x.intersection(A)
-lenA = len(a)
-b = x.intersection(B)
-lenB = len(b)
-happiness = lenA - lenB
-print(happiness)
+x = 0
+for i in arr:
+    if(i in A):
+        x = x + 1
+    elif(i in B):
+        x = x - 1
+    else:
+        continue
+print(x)

584f2a561c68e669927f60f54b07e1874ae8d0c9
Ananya <ananyaunique21@gmail.com>
2022-04-13 07:45:15 +0530

    Numpy in Python

--- HackerRank/Python/Numpy/Shape _and_Reshape.py
+++ HackerRank/Python/Numpy/Shape _and_Reshape.py
@@ -1,5 +1,9 @@
 import numpy
 
-x = numpy.arange(1,10).reshape(3,3)
+x = numpy.array(list(map(int, input().split())))
+x = x.reshape(3,3)
 print(x)
+
+# x = numpy.arange(1,10).reshape(3,3)
+# print(x)
 

97c10f5ee8912e12439c34a1b28407b2c9356b15
Ananya <ananyaunique21@gmail.com>
2022-05-08 00:34:39 +0530

    Day 1-2.py

--- HackerRank/Tutorials/10 Days Of Statistics/Day 1-2.py
+++ HackerRank/Tutorials/10 Days Of Statistics/Day 1-2.py
@@ -1,32 +1,42 @@
-from statistics import median
-N,arr = int(input()),[]
-data,freq = list(map(int,input().split())),list(map(int,input().split()))
-for i in range(N):
-        arr += [data[i]] * freq[i]
-arr.sort()
-t = int(len(arr)/2)
-if len(arr)%2==0:
-        q1=arr[:t]
-        q3=arr[t:]
-else:
-        q1=arr[:t]
-        q3=arr[t+1:]
-print(round(float(median(q3)-median(q1)), 1))
+#!/bin/python3
 
-#or
-
+import math
+import os
+import random
+import re
+import sys
 import statistics as st
-n = int(input())
-X = [int(i) for i in input().split()]
-F = [int(i) for i in input().split()]
 
-S= []
-for i in range(n):
-    S.extend([X[i]]*F[i])
+# Complete the 'interQuartile' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY values
+#  2. INTEGER_ARRAY freqs
+#
 
-S = sorted(S)
+def interQuartile(values, freqs):
+    # Print your answer to 1 decimal place within this function
+    
+    new = []
+    for i in range(len(values)):
+        for j in range(freqs[i]):
+            new.append(values[i])
+    s = sorted(new)
+    
+    if len(s)%2 != 0:
+       q1 = st.median(s[:len(s)//2])
+       q3 = st.median(s[len(s)//2+1:])
+    else:
+       q1 = st.median(s[:len(s)//2])
+       q3 = st.median(s[len(s)//2:])
+    
+    iqr = q3-q1
+    print("{:.1f}".format(iqr))    
+  
+if __name__ == '__main__':
+    n = int(input().strip())
+    val = list(map(int, input().rstrip().split()))
+    freq = list(map(int, input().rstrip().split()))
+    interQuartile(val, freq)
 
-
-n = len(S)
-n2 = int(n/2)
-print(round(float(st.median(S[-n2:])-st.median(S[:n2])),1))
+    

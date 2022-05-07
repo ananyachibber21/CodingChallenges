75bb5ff0b5484a3310bbc10effe07d17cf1af1a8
Ananya <ananyaunique21@gmail.com>
2022-05-07 23:45:31 +0530

    Update Day 1-2.py

--- HackerRank/Tutorials/10 Days Of Statistics/Day 1-2.py
+++ HackerRank/Tutorials/10 Days Of Statistics/Day 1-2.py
@@ -12,3 +12,21 @@ else:
         q1=arr[:t]
         q3=arr[t+1:]
 print(round(float(median(q3)-median(q1)), 1))
+
+#or
+
+import statistics as st
+n = int(input())
+X = [int(i) for i in input().split()]
+F = [int(i) for i in input().split()]
+
+S= []
+for i in range(n):
+    S.extend([X[i]]*F[i])
+
+S = sorted(S)
+
+
+n = len(S)
+n2 = int(n/2)
+print(round(float(st.median(S[-n2:])-st.median(S[:n2])),1))
